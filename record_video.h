fb48526879f71e06892fff20bb6abb8a130552b8
samrussell-code <sc22s3r@leeds.ac.uk>
2023-12-01 18:47:54 +0000

    merged camera select box from sam branch into dev

--- record_video.h
+++ record_video.h
@@ -1,6 +1,7 @@
 #ifndef RECORDVIDEO_H
 #define RECORDVIDEO_H
 
+#include "qcamerainfo.h"
 #include <QMainWindow>
 #include <QtMultimedia/QCamera>
 #include <QtMultimediaWidgets/QCameraViewfinder>
@@ -8,10 +9,14 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QPushButton>
+#include <QComboBox>
+
+Q_DECLARE_METATYPE(QCameraInfo)
 
 class RecordVideo : public QMainWindow
 {
     Q_OBJECT
+
 
 public:
     explicit RecordVideo(QWidget *parent = nullptr);
@@ -19,18 +24,28 @@ public:
 
 private:
     void setupCamera();
+    void updateCameraSettings();
+    void updateViewfinderSettings();
 
     QCamera *camera;
     QCameraViewfinder *viewfinder;
     QMediaRecorder *recorder;
-
     QVBoxLayout *leftLayout;
     QVBoxLayout *rightLayout;
     QVBoxLayout *eventInfoLayout;
 
-    QLabel *label;
-    QPushButton *searchButton;
-    QPushButton *closeButton;
+    QLabel *schedule_time;
+    QPushButton *recordButton;
+    QPushButton *flipButton;
+    QPushButton *toggleModeButton;
+    QComboBox *cameraComboBox;
+
+    bool verticalMode;
+    QCameraInfo currentCameraInfo;
+
+private slots:
+    void toggleRecordingMode();
+    void switchCamera(int index);
 };
 
-#endif // MAINWINDOW_H
+#endif // RECORDVIDEO_H
